9ae32d284dea4b21cbe695767432115a898baa1f
Okechukwu <okei.muokwugwo@outlook.com>
2022-11-03 20:54:54 +0100

    Update App.js component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import NavBar from './components/NavBar';
+// Importing pages
+import Home from './components/pages/Home';
+import Quotes from './components/pages/Quotes';
 
-// Create App class initialize with props
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+// Create App function
+const App = () => (
+  <div className="App">
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/quotes" element={<Quotes />} />
+      <Route path="/calculator" element={<Calculator />} />
+    </Routes>
+  </div>
+);
 
 export default App;
