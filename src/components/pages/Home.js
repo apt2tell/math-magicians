02655da6875dea6f29a4e8520f25d56b02c5ce2e
Okechukwu <okei.muokwugwo@outlook.com>
2022-11-03 20:52:41 +0100

    Create Home component of the app

---
+++ src/components/pages/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './pages.css';
+
+const Home = () => (
+  <div className="home">
+    <h2 className="welcome">Welcome to our page!</h2>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia deleniti
+      commodi cupiditate obcaecati inventore, repellat minus facilis eligendi
+      sed delectus, voluptates aliquam impedit, quaerat aperiam architecto
+      nesciunt nisi recusandae rerum corrupti unde. Dolorem nulla eaque in
+      quisquam, ut minus, aperiam consequatur odio laudantium iusto fugiat
+      repudiandae deleniti dolores ab consectetur a mollitia eos quam natus?
+      Repudiandae officiis fuga consectetur, impedit sed eligendi aperiam,
+      velit, quidem hic ex perferendis culpa aut illum! Nisi, iste. Officia
+      veniam quis doloribus? Officiis, accusantium nesciunt.
+    </p>
+    <br />
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia deleniti
+      commodi cupiditate obcaecati inventore, repellat minus facilis eligendi
+      sed delectus, voluptates aliquam impedit, quaerat aperiam architecto
+      nesciunt nisi recusandae rerum corrupti unde. Dolorem nulla eaque in
+      quisquam, ut minus, aperiam consequatur odio laudantium iusto fugiat
+      repudiandae deleniti dolores ab consectetur a mollitia eos quam natus?
+      Repudiandae officiis fuga consectetur, impedit sed eligendi aperiam,
+      velit, quidem hic ex perferendis culpa aut illum! Nisi, iste. Officia
+      veniam quis doloribus? Officiis, accusantium nesciunt.
+    </p>
+  </div>
+);
+
+export default Home;
