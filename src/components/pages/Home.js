108ca5662c8a5ad0b2d0586758757c6a1b3d153f
Okechukwu Muokwugwo <okei.muokwugwo@gmail.com>
2023-07-03 14:50:39 +0100

    Add styles to Home component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,31 +2,41 @@ import React from 'react';
 import './pages.css';
 
 const Home = () => (
-  <div className="home">
-    <h2 className="welcome">Welcome to our page!</h2>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia deleniti
-      commodi cupiditate obcaecati inventore, repellat minus facilis eligendi
-      sed delectus, voluptates aliquam impedit, quaerat aperiam architecto
-      nesciunt nisi recusandae rerum corrupti unde. Dolorem nulla eaque in
-      quisquam, ut minus, aperiam consequatur odio laudantium iusto fugiat
-      repudiandae deleniti dolores ab consectetur a mollitia eos quam natus?
-      Repudiandae officiis fuga consectetur, impedit sed eligendi aperiam,
-      velit, quidem hic ex perferendis culpa aut illum! Nisi, iste. Officia
-      veniam quis doloribus? Officiis, accusantium nesciunt.
-    </p>
-    <br />
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia deleniti
-      commodi cupiditate obcaecati inventore, repellat minus facilis eligendi
-      sed delectus, voluptates aliquam impedit, quaerat aperiam architecto
-      nesciunt nisi recusandae rerum corrupti unde. Dolorem nulla eaque in
-      quisquam, ut minus, aperiam consequatur odio laudantium iusto fugiat
-      repudiandae deleniti dolores ab consectetur a mollitia eos quam natus?
-      Repudiandae officiis fuga consectetur, impedit sed eligendi aperiam,
-      velit, quidem hic ex perferendis culpa aut illum! Nisi, iste. Officia
-      veniam quis doloribus? Officiis, accusantium nesciunt.
-    </p>
+  <div className="home-contianer">
+    <div className="home-content">
+      <h2 className="home-welcome">Welcome to our page!</h2>
+      <p className="home-text">
+        Mathematics is a discipline that has shaped the very essence of human understanding.
+        It has its roots in the ancient civilizations of Mesopotamia, Egypt, and the Indus Valley,
+        where people needed to count, measure, and understand the patterns and
+        phenomena of the world around them.
+        The Sumerians of Mesopotamia devised a numeral system based
+        on the concept of place value,
+        with a base of 60. This system gave rise to the measurement of time, angles,
+        and the division of a circle into 360 degrees.
+        The ancient Greeks elevated mathematics to new heights.
+        Pythagoras introduced the concept of numbers having both
+        quantitative and qualitative properties.
+        His theorem, relating the lengths of the sides of a right-angled triangle,
+        still stands as one of the fundamental principles of geometry.
+
+      </p>
+      <p className="home-text">
+        Euclid compiled his magnum opus, Elements,
+        which laid down the foundations of rigorous deductive reasoning in mathematics.
+        Euclidean geometry provided a framework for understanding spatial
+        relationships, shapes, and proofs,
+        which would become a cornerstone of mathematical study for centuries to come.
+        In the 17th century, the brilliant mathematician Isaac Newton developed calculus,
+        a mathematical framework that enabled the study of change and motion. Newtons ideas,
+        along with the contributions of other mathematicians like Gottfried Leibniz,
+        paved the way for modern physics and engineering.
+        Mathematics continues to evolve and expand its reach. It underpins the fields of physics,
+        economics, cryptography, and many others.
+        It is a powerful tool that allows us to understand the world around us and to solve problems
+        in ways that would not be possible without it.
+      </p>
+    </div>
   </div>
 );
 
